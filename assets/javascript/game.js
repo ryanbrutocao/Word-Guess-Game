9085342fc024dd987d87147ea3669f0c935d5bf1
Ryan Brutocao <ryanbrutocao@outlook.com>
2019-07-02 16:01:13 -0600

    updating with working word to dash code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,7 +6,17 @@ console.log("randomly chosen word:", randomWord);
 
 //not sure if I'll need this
 var wordLength=randomWord.length;
-console.log("var wordLength= :", wordLength);
+// console.log("var wordLength= :", wordLength);
+document.getElementById("currentWordText").innerHTML=wordLength;
+
+// --------
+var sentence = randomWord;
+
+for (let i=0; i<randomWord.length; i++){
+console.log(sentence.charAt(i));
+}
+// ----------------
+
 
 var splitWord=randomWord.split();
 console.log("var splitWord: = ", splitWord);
@@ -14,34 +24,74 @@ console.log("var splitWord: = ", splitWord);
 var splitWord=randomWord.split("");
 console.log("var quoted splitWord: = ", splitWord);
 
-//----------parses the random word into multiple characters---//
-var sentence = randomWord;
+// -----------
+function dashesWord(randomWord) {
+    var dashes = []
+    for (let i=0; i<randomWord.length; i++) {
+      dashes.push("_");  
+    }
+    console.log(dashes);
+    console.log(randomWord);
+  }
+  dashesWord(splitWord);
+  // ---------------
+  
+//   var wordDashFunct=function dashes(randomWord) {
+//     var dashes = []
+//     for (let i=0; i<randomWord.length; i++) {
+//       dashes.push("_");  
+//     }
+//     document.getElementById("#dashText").innerHTML=wordDashFunct(splitWord);
+// }
+//  wordDashFunct(splitWord);
 
-for (let i=0; i<randomWord.length; i++){
-console.log(sentence.charAt(i));
-}
+
+//show this on my main screen
+
+
+
+//----------parses the random word into multiple characters---//
 
 
 // -------------------- Good working code above------------------//
 
 
 
-var dashes = "";
 
 
-for (i = 0; i < randomWord.length; i++) {
-  if (randomWord.charAt(i) == " ") {
-    dashes += " ";
-  } else {
-    dashes += "-";
-  }
-}
-console.log("dashes:", dashes)
+
+
+
+// input letter-  if that matches something in the array, then we want to change from a dash to show the actual letter else remove a count from the guesses.
+
+// function (input letter is user key)
+// if { user key matches switch dash for letter.
+
+// } else
+// remove a guess from the user guess. decerment i--;
+
+
+
+
+
+// var dashes = "";
+
+
+// for (i = 0; i < randomWord.length; i++) {
+//   if (randomWord.charAt(i) == " ") {
+//     dashes += " ";
+//   } else {
+//     dashes += "-";
+//   }
+// }
+// console.log("dashes:", dashes)
+
+
 // expected output: "The character at index 4 is q"
 
 
 // places the length of the word-- want to exchange 'wordLenght with a function that changes the letters to dashes
-document.getElementById("currentWordText").innerHTML=wordLength;
+// document.getElementById("currentWordText").innerHTML=wordLength;
 
 
 //function that changes letter to dashes (replace()) https://www.w3schools.com/jsref/jsref_replace.asp
@@ -60,11 +110,11 @@ document.getElementById("currentWordText").innerHTML=wordLength;
 //   }
 //   -------------------
 
-var array1 = ["apple", "banana", "carrot", "durian"];
+// var array1 = ["apple", "banana", "carrot", "durian"];
 
 
-var map1 = randomWord.split("");
-console.log(map1);
+// var map1 = randomWord.split("");
+// console.log(map1);
 
 // // replace position 1 of array with a dashes, position 2 with a dash etc
 // function myFunction (i=0; i<randomWord.length; i++)  {
